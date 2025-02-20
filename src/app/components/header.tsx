5ff84c41e9ed241ad56625ba9e26f335aee60610
FlashDC7 <jrda8303@gmail.com>
2025-02-20 16:52:16 -0600

    ajustes css v1.1.0

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React, { useEffect } from "react";
+// import React, { useEffect } from "react";
 import logo from "../media/logo-solucionic.png";
 import imgCalculadora from "../media/header-calculadora.png";
 import Image from "next/image";
