921e3fa98efe8015ef935e372441322e98ecc515
AdanTouchPoint <adan.mijangos@touchpointmarketing.mx>
2025-02-21 14:52:37 -0600

    fix: select input in step2

--- src/app/components/SecondStep.tsx
+++ src/app/components/SecondStep.tsx
@@ -6,8 +6,8 @@ return(
 <label className="text-xs block text-gray-700 font-semibold">Motivo de la separación</label>
 <select className="w-full border rounded-lg p-3 mt-1 text-gray-400 focus:ring-2 focus:ring-red-400 text-[#848484]">
   <option>Separación Voluntaria</option>
-  <option>Despido Justificado</option>
-  <option>Despido Injustificado</option>
+  <option>Término de contrato Justificado</option>
+  <option>Otra que no es despido</option>
 </select>
   </div>
 </div>
